fdc40f2d1e796d61cbd6795644fbb9b8babd8acd
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2021-02-18 01:50:12 +0900

    add tests

--- test/test.cpp
+++ test/test.cpp
@@ -29,18 +29,39 @@ TEST(ratio, minus)
     EXPECT_EQ((-a).denominator, 1);
 }
 
-TEST(ratio, reduce_member_1)
+TEST(ratio, reduce_member)
 {
     ri a(2, 2);
     EXPECT_EQ(a.reduce(), ri(1, 1));
     EXPECT_EQ(a, ri(1, 1));
 }
 
-TEST(ratio, reduce_member_2)
+TEST(ratio, reduce_func)
+{
+    ri a(1, -1);
+    EXPECT_EQ(ratio::reduce(a), ri(-1, 1));
+    EXPECT_EQ(a, ri(1, -1));
+}
+
+TEST(ratio, reduce_func_minus)
 {
     ri a(1, -1);
     EXPECT_EQ(a.reduce(), ri(-1, 1));
-    EXPECT_EQ(a, ri(-1, 1));
+    EXPECT_EQ(a, ri(1, -1));
+}
+
+TEST(ratio, reduce_func_numerator_zero)
+{
+    ri a(0, 2);
+    EXPECT_EQ(a.reduce(), ri(0, 1));
+    EXPECT_EQ(a, ri(0, 2));
+}
+
+TEST(ratio, reduce_func_denominator_zero)
+{
+    ri a(2, 0);
+    EXPECT_EQ(a.reduce(), ri(1, 0));
+    EXPECT_EQ(a, ri(2, 0));
 }
 
 TEST(ratio, inverse_member)
@@ -50,7 +71,7 @@ TEST(ratio, inverse_member)
     EXPECT_EQ(a, ri(2, 1));
 }
 
-TEST(ratio, inverse)
+TEST(ratio, inverse_func)
 {
     ri a(1, 2);
     EXPECT_EQ(ratio::inverse(a), ri(2, 1));
@@ -61,6 +82,13 @@ TEST(ratio, add)
 {
     ri a(1, 2);
     EXPECT_EQ(a + a, ri(1, 1));
+    EXPECT_EQ(a, ri(1, 2));
+}
+
+TEST(ratio, subtract)
+{
+    ri a(1, 2);
+    EXPECT_EQ(a - a, ri(0, 1));
     EXPECT_EQ(a, ri(1, 2));
 }
 
@@ -76,6 +104,21 @@ TEST(ratio, divide)
     ri a(1, 2);
     EXPECT_EQ(a / a, ri(1, 1));
     EXPECT_EQ(a, ri(1, 2));
+}
+
+TEST(ratio, to_double_member)
+{
+    EXPECT_DOUBLE_EQ(ri(1, 2).to_double(), 1. / 2);
+}
+
+TEST(ratio, to_double_func)
+{
+    EXPECT_DOUBLE_EQ(ratio::to_double(ri(1, 2)), 1. / 2);
+}
+
+TEST(ratio, to_double_func_nan)
+{
+    EXPECT_DOUBLE_EQ(ratio::to_double(ri(1, 0)), std::numeric_limits<double>::infinity());
 }
 
 int main(int argc, char **argv) {
